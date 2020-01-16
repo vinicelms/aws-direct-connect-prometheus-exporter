58b55a1b69a07a2e89927f1c170633d66783aa78
Vinicius Celms <vinicius.celms@gmail.com>
2020-01-16 14:24:46 -0300

    Add Regions options to config parser

--- core/config.py
+++ core/config.py
@@ -5,6 +5,7 @@ class Config:
     def __init__(self):
         self.__aws_access_key = self.__get_aws_access_key()
         self.__aws_secret_key = self.__get_aws_secret_key()
+        self.__aws_regions = None
 
     @property
     def aws_access_key(self):
@@ -13,6 +14,10 @@ class Config:
     @property
     def aws_secret_key(self):
         return self.__aws_secret_key
+
+    @property
+    def aws_region(self):
+        return self.__aws_regions
 
     def __get_aws_access_key(self):
         if 'AWS_ACCESS_KEY' not in os.environ:
@@ -25,3 +30,9 @@ class Config:
             raise EnvironmentError("Environment variable AWS_SECRET_KEY has not been set")
         else:
             return os.environ['AWS_SECRET_KEY']
+
+    def __get_aws_regions(self):
+        if 'AWS_REGIONS' not in os.environ:
+            raise EnvironmentError("Environment variable AWS_REGIONS has not been set")
+        else:
+            return os.environ['AWS_REGIONS']
