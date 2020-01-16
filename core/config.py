90a58428ff43e9f9ae8e6583ec7aea967e3108a5
Vinicius Celms <vinicius.celms@gmail.com>
2020-01-16 19:57:36 -0300

    Adding region config to obtain Direct Connect info

--- core/config.py
+++ core/config.py
@@ -9,6 +9,7 @@ class Config:
     def __init__(self):
         self.__aws_access_key = self.__get_aws_access_key()
         self.__aws_secret_key = self.__get_aws_secret_key()
+        self.__aws_region = self.__get_aws_region()
 
     @property
     def aws_access_key(self):
@@ -17,6 +18,10 @@ class Config:
     @property
     def aws_secret_key(self):
         return self.__aws_secret_key
+
+    @property
+    def aws_region(self):
+        return self.__aws_region
 
     def __get_aws_access_key(self):
         if 'AWS_ACCESS_KEY' not in os.environ:
@@ -33,3 +38,11 @@ class Config:
         else:
             logging.info("AWS_SECRET_KEY environment variable returned!")
             return os.environ['AWS_SECRET_KEY']
+
+    def __get_aws_region(self):
+        if 'AWS_REGION' not in os.environ:
+            logging.critical("Environment variable AWS_REGION has not been set")
+            raise EnvironmentError("Environment variable AWS_REGION has not been set")
+        else:
+            logging.info("AWS_REGION environment variable returned!")
+            return os.environ['AWS_REGION']
