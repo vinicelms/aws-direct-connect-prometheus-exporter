d3ec1c6a1f128015593484e11b384cd53c7557c3
Vinicius Celms <vinicius.celms@gmail.com>
2020-01-16 14:42:51 -0300

    Removing region configuration as it is unnecessary

--- core/config.py
+++ core/config.py
@@ -5,7 +5,6 @@ class Config:
     def __init__(self):
         self.__aws_access_key = self.__get_aws_access_key()
         self.__aws_secret_key = self.__get_aws_secret_key()
-        self.__aws_region = self.__get_aws_region()
 
     @property
     def aws_access_key(self):
@@ -14,10 +13,6 @@ class Config:
     @property
     def aws_secret_key(self):
         return self.__aws_secret_key
-
-    @property
-    def aws_region(self):
-        return self.__aws_region
 
     def __get_aws_access_key(self):
         if 'AWS_ACCESS_KEY' not in os.environ:
@@ -30,9 +25,3 @@ class Config:
             raise EnvironmentError("Environment variable AWS_SECRET_KEY has not been set")
         else:
             return os.environ['AWS_SECRET_KEY']
-
-    def __get_aws_region(self):
-        if 'AWS_REGION' not in os.environ:
-            raise EnvironmentError("Environment variable AWS_REGION has not been set")
-        else:
-            return os.environ['AWS_REGION']
