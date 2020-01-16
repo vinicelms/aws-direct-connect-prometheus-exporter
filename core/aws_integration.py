61e0eee2e7caecce5fc5e7ca9a8f0def90f6fffd
Vinicius Celms <vinicius.celms@gmail.com>
2020-01-16 14:31:18 -0300

    AWS integration base structure

---
+++ core/aws_integration.py
@@ -0,0 +1,24 @@
+import boto3
+from config import Config
+
+CONFIG = Config()
+
+class AWSInfo:
+
+    def __init__(self):
+        pass
+
+class DirectConnect:
+
+    def __init__(self):
+        pass
+
+class VirtualInterface:
+
+    def __init__(self):
+        pass
+
+class BGP:
+
+    def __init__(self):
+        pass
